ee4877759d3953ecf9028098893257414e68b50b
andyoe0102 <oe.andy0102@gmail.com>
2020-06-20 09:45:16 -0500

    first commit

--- MongoDB/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/airbnb');
-
-let reviewSchema = mongoose.Schema({
-
-});
-
-let Reviews = mongoose.model('Reviews',reviewSchema);
-
--- client/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/app.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-
-ReactDOM.render(<App />, document.getElementById('app'));
